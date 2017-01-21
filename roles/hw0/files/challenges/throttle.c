6d100619f285eac4b1aaabb5306e716f14ff3111
ctinray <b03902072@ntu.edu.tw>
2017-01-21 15:38:34 +0800

    add throttle

--- roles/hw0/files/challenges/throttle.c
+++ roles/hw0/files/challenges/throttle.c
@@ -1,13 +1,20 @@
+#define _XOPEN_SOURCE
+
 #include <stdio.h>
-#include <signal.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include "decrypt.h"
 
-int sleep = 0, quit = 0, sigint = 0; 
+int sigsleep = 0, sigquit = 0, sigint = 0; 
 
-void handler(int signal) {
-  switch (signal) {
-  case SIGSTOP:
-    sleep = 1;
+void handler(int sig) {
+  printf("\n\n\n\n\n\n");
+
+  switch (sig) {
+  case SIGTSTP:
+    sigsleep = 1;
     printf("slept...\n");
     break;
   case SIGINT:
@@ -15,28 +22,43 @@ void handler(int signal) {
     printf("interrupted...\n");
     break;
   case SIGQUIT:
-    quit = 1;
+    sigquit = 1;
     printf("quited...\n");
     break;
   }
 
-  if (sleep == 0 || quit == 0 || sigint == 0) {
-    printf("Give me more signam :D\n");
+  if (sigsleep == 0 || sigquit == 0 || sigint == 0) {
+    printf("Give me more signal :D\n");
   } else {
     char flag[] = FLAG;
     decrypt(flag);
     printf("%s\n", flag);
+    exit(0);
   }
-  
+  sleep(3);
 }
 
 
 int main() {
-  signal(SIGHUP, handler); 
+  signal(SIGTSTP, handler); 
   signal(SIGINT, handler);
   signal(SIGQUIT, handler);
+  struct sigaction sa;
+  sa.sa_handler = handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
 
-  while (true) {
+  int res = 1;
+  res *= sigaction(SIGINT, &sa, NULL);
+  res *= sigaction(SIGQUIT, &sa, NULL);
+  res *= sigaction(SIGSTOP, &sa, NULL);
+    
+  if (sigaction(SIGINT, &sa, NULL) < 0) {
+    printf("sigaction fail, please contact us vegetable@csie.ntu.edu.tw");
+    return -1;
+  }
+  
+  while (1) {
     printf("fly fly fly\n");
   }
   
